0afa8085cd3957cc239e7615ff111e428892e674
Atif8Ted <mohd.atifimam@hotmail.com>
2018-04-11 11:23:55 +0530

    Updated repo

--- src/day4/DestructorClassMain.java
+++ src/day4/DestructorClassMain.java
@@ -8,8 +8,8 @@ public class DestructorClassMain {
 		dd.f1();
 		dd=null;
 		DestructorClass dd1 =dd;
-		dd1=null;
 		dd=null;
+		dd1=null;
 		new DestructorClass().f1(); // automaticall goes to garbage collection
 		System.gc(); // if we dont call Sysye.gc() then garbage collector will not collect immediatory  it will do the garbage collection acordint to its mind.
 		//but when we do gc() it will come instantly garbage collector.
